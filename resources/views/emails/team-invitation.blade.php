838f94040fa4120027482db6acaf29dbdd11ffde
Salehuddin <salehuddin.a@gmail.com>
2026-01-29 11:59:59 +0800

    Add missing team invitation email template

--- resources/views/emails/team-invitation.blade.php
+++ resources/views/emails/team-invitation.blade.php
@@ -1,14 +1,14 @@
-<x-mail::message>
-# You've been invited!
+@component('mail::message')
+# You've Been Invited!
 
-**{{ $inviter->name }}** has invited you to join **{{ $organization->name }}** on QRCode Platform.
+{{ $inviter->name }} has invited you to join **{{ $organization->name }}** on QRCode Platform.
 
-<x-mail::button :url="$url">
+@component('mail::button', ['url' => $url])
 Accept Invitation
-</x-mail::button>
+@endcomponent
 
-If you did not expect this invitation, you can ignore this email.
+This invitation will expire in 7 days.
 
 Thanks,<br>
 {{ config('app.name') }}
-</x-mail::message>
+@endcomponent
